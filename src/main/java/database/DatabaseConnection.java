ba978931332cc7253b1ea8829785bae5efab52cd
mateusz <mateusznieweglowski10@gmail.com>
2025-05-27 22:17:27 +0200

    site

--- src/main/java/database/DatabaseConnection.java
+++ src/main/java/database/DatabaseConnection.java
@@ -1,33 +1,47 @@
 package database;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private java.sql.Connection connection;
 
-    public Connection getConnection() {
-        return connection;
+    static private final Map<String, Connection> connections = new HashMap<>();
+
+    static public Connection getConnection() {
+        return getConnection("");
     }
 
-    public void connect(String dbPath){
-        try{
-            String url = "jdbc:sqlite:" + dbPath;
-            this.connection = DriverManager.getConnection(url);
-            System.out.println("Database connected");
+    static public Connection getConnection(String name) {
+        return connections.get(name);
+    }
+
+    static public void connect(String filePath) {
+        connect(filePath, "");
+    }
+
+    static public void connect(String filePath, String connectionName){
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:" + filePath);
+            connections.put(connectionName, connection);
         } catch (SQLException e) {
-            System.err.println("Connect error" + e.getMessage());
+            throw new RuntimeException(e);
         }
     }
-    public void disconnect(){
-        try{
-            if(this.connection != null && !this.connection.isClosed()){
-                this.connection.close();
-                System.out.println("Closed connection with database");
-            }
+
+    static public void disconnect() {
+        disconnect("");
+    }
+
+    static public void disconnect(String connectionName){
+        try {
+            Connection connection = connections.get(connectionName);
+            connection.close();
+            connections.remove(connectionName);
         } catch (SQLException e) {
-            System.err.println("Disconnect error" + e.getMessage());
+            throw new RuntimeException(e);
         }
     }
 }
